74bc50c6c6ee767385770f4730f867c1b634d7b3
Ramrekha yadav <94756876+Ramyadav08@users.noreply.github.com>
2024-02-05 09:54:46 +0530

    Update settings.py
    latest changes

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-(al=!)e45g545irm9%yr^3)(w0ms5c=s6)p9p+eeszlllm*(sc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
